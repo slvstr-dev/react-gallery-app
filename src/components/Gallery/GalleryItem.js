1336f5ba9cc7efc7c2d626acc4d48d41e47449b7
Sylvester Hofstra <hello@slvstr.dev>
2021-04-09 17:29:27 +0200

    Implement PropTypes.

--- src/components/Gallery/GalleryItem.js
+++ src/components/Gallery/GalleryItem.js
@@ -1,9 +1,15 @@
+import PropTypes from "prop-types";
+
 const GalleryItem = ({ src }) => {
     return (
         <li>
             <img src={src} alt="" />
         </li>
     );
+};
+
+GalleryItem.propTypes = {
+    src: PropTypes.string.isRequired,
 };
 
 export default GalleryItem;
--- src/components/Nav/NavItem.js
+++ src/components/Nav/NavItem.js
@@ -1,9 +1,16 @@
+import PropTypes from "prop-types";
+
 const NavItem = ({ href, category }) => {
     return (
         <li>
             <a href={href}>{category}</a>
         </li>
     );
+};
+
+NavItem.propTypes = {
+    href: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
 };
 
 export default NavItem;
